6055a00b9aa57328c03b29315c24fb56f6dfd443
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-12-01 08:39:22 +0100

    step 1

--- day_1/decode.py
+++ day_1/decode.py
@@ -1,10 +1,39 @@
+validnumbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0,
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "zero",
+]
+
+
 def getinput():
     with open("input.txt", "r") as f:
         return f.read().splitlines()
 
 
-def decode(line):
-    print(line)
+def decode(line: str):
+    found_numbers = []
+    for validnumber in validnumbers:
+        if str(validnumber) in line:
+            line = line.replace(str(validnumber), "")
+            found_numbers.append(validnumber)
+    print(found_numbers)
 
 
 def main():
