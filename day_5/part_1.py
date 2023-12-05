e1a95ac2c468ed1721aaa80f1350e7dae5dfc894
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-12-05 14:48:47 +0100

    part 1 approach does not work

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -35,11 +35,19 @@ humidity-to-location map:
 """
 
 
-# converts things like [[50, 98, 2],[52, 50, 48]] to [[range(first,first+last),range(second,second+last)],[range(first,first+last),range(second,second+last)]]
+# # converts things like
+# """
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+# """
+# to [[range(first,first+last),range(second,second+last)],[range(first,first+last),range(second,second+last)]]
 # (in this case [[range(50,50+2),range(98,98+2)],[range(52,52+48),range(50,50+48)]]) or [[[50,51],[98,99]],[52,53,54,55,...,(52+48-1)],[50,51,52,53,...,(50+48-1)]]]
 def generate_map(map: str):
     lines = map.split("\n")
     numberlines = lines[1:]
+    if numberlines[-1] == "":
+        numberlines = numberlines[:-1]
     # converts the lines to lists of numbers
     for i in range(len(numberlines)):
         numbers = numberlines[i].split(" ")
@@ -53,7 +61,7 @@ def generate_map(map: str):
         firstrange = range(numberlines[i][0], numberlines[i][0] + numberlines[i][2])
         secondrange = range(numberlines[i][1], numberlines[i][1] + numberlines[i][2])
         numberlines[i] = [firstrange, secondrange]
-
+    data = []
     # converts the lists of ranges to lists of numbers
     for i in range(len(numberlines)):
         firstrange = numberlines[i][0]
@@ -64,29 +72,39 @@ def generate_map(map: str):
             firstlist.append(j)
         for j in secondrange:
             secondlist.append(j)
-        numberlines[i] = [firstlist, secondlist]
+        data.append([firstlist, secondlist])
+    return data
 
-    return numberlines
 
-
-def calculate_soil(seed, seed_to_soil_map):
-    destinationranges = []
-    sourceranges = []
+def calculate_value(value, value_to_range_map):
+    if value_to_range_map is None or value_to_range_map == []:
+        print("empty_map")
+        return value
+    destination_ranges = []
+    source_ranges = []
     index = None
-    for i in range(len(seed_to_soil_map)):
-        if seed in seed_to_soil_map[i][0]:
-            destinationranges.append(seed_to_soil_map[i][1])
-    for i in range(len(seed_to_soil_map)):
-        if seed in seed_to_soil_map[i][1]:
-            sourceranges.append(seed_to_soil_map[i][0])
-    for i in range(len(destinationranges)):
-        if seed in destinationranges[i]:
-            index = destinationranges[i].index(seed)
-    if index != None:
-        if index in range(len(sourceranges[0])):
-            return sourceranges[0][index]
+    for i in range(len(value_to_range_map)):
+        if value in value_to_range_map[i][0]:
+            destination_ranges.append(value_to_range_map[i][1])
+    for i in range(len(value_to_range_map)):
+        if value in value_to_range_map[i][1]:
+            source_ranges.append(value_to_range_map[i][0])
+    if len(destination_ranges) == 0:
+        print("error in calculate_value (destination_ranges)")
+        return value
+    if len(source_ranges) == 0:
+        print("error in calculate_value (source_ranges)")
+        return value
+    for i in range(len(destination_ranges)):
+        if value in destination_ranges[i]:
+            index = destination_ranges[i].index(value)
+
+    if index is not None:
+        if index in range(len(source_ranges[0])):
+            return source_ranges[0][index]
     else:
-        index = seed
+        print("error")
+        index = value
     return index
 
 
@@ -101,8 +119,37 @@ def calculate_seeds(
     humidity_to_location_map,
 ):
     # calculate soil
-    soil = calculate_soil(seed, seed_to_soil_map)
-    print("soil is " + str(soil), "for seed", seed)
+    print("calculating soil")
+    soil = calculate_value(seed, seed_to_soil_map)
+    # calculate fertilizer
+    print("calculating fertilizer")
+    fertilizer = calculate_value(soil, soil_to_fertilizer_map)
+    # calculate water
+    print("calculating water")
+    water = calculate_value(fertilizer, fertilizer_to_water_map)
+    # calculate light
+    print("calculating light")
+    light = calculate_value(water, water_to_light_map)
+    # calculate temperature
+    print("calculating temperature")
+    temperature = calculate_value(light, light_to_temperature_map)
+    # calculate humidity
+    print("calculating humidity")
+    humidity = calculate_value(temperature, temperature_to_humidity_map)
+    # calculate location
+    print("calculating location")
+    location = calculate_value(humidity, humidity_to_location_map)
+
+    print(
+        "seed: " + str(seed),
+        "soil: " + str(soil),
+        "fertilizer: " + str(fertilizer),
+        "water: " + str(water),
+        "light: " + str(light),
+        "temperature: " + str(temperature),
+        "humidity: " + str(humidity),
+        "location: " + str(location),
+    )
 
 
 def main():
@@ -111,7 +158,6 @@ def main():
     seeds = lines[0].split(": ")[1].split(" ")
     for i in range(len(seeds)):
         seeds[i] = int(seeds[i])
-    print(seeds)
     seed_to_soil_map = generate_map(lines[1])
     soil_to_fertilizer_map = None
     fertilizer_to_water_map = None
@@ -120,12 +166,12 @@ def main():
     temperature_to_humidity_map = None
     humidity_to_location_map = None
 
-    # soil_to_fertilizer_map = generate_map(lines[2])
-    # fertilizer_to_water_map = generate_map(lines[3])
-    # water_to_light_map = generate_map(lines[4])
-    # light_to_temperature_map = generate_map(lines[5])
-    # temperature_to_humidity_map = generate_map(lines[6])
-    # humidity_to_location_map = generate_map(lines[7])
+    soil_to_fertilizer_map = generate_map(lines[2])
+    fertilizer_to_water_map = generate_map(lines[3])
+    water_to_light_map = generate_map(lines[4])
+    light_to_temperature_map = generate_map(lines[5])
+    temperature_to_humidity_map = generate_map(lines[6])
+    humidity_to_location_map = generate_map(lines[7])
 
     # run the simulation
     for i in range(len(seeds)):
